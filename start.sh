264732517e6147e884e785dd392420ae2c163189
lalfaro2 <alfaro.luis123@gmail.com>
2023-02-06 10:36:34 -0500

    working on start.sh

--- start.sh
+++ start.sh
@@ -7,3 +7,8 @@ terraform "$1" --auto-approve \
 -var env="$3" \
 -var github-repo="$4" \
 -var codestar-connection-arn="$5"
+
+if [ "$1" = "destroy" ]; then
+    aws iam delete-role --role-name "$6"
+    aws lambda delete-function --function-name "$7"
+fi
